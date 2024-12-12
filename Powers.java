21c2326577207c90875abc390aa8859f2f3c5baf
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 21:07:35 +0530

    Add files via upload

---
+++ Powers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Powers{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a&(a-2))==0){
+            System.out.println("power of 4");
+        }
+        else{
+            System.out.println("not a power of 4");
+        }
+    }
+}
