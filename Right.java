9dd091503275f61e50d19466a7be4c9fb72ad4a0
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 21:07:06 +0530

    Add files via upload

---
+++ Right.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Right{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int exp=a/(int)Math.pow(2,b);
+        System.out.println(exp);
+    }
+}
