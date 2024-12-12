b955e4bbbcfbbce70fa35a5c1e814f9ff226e86e
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 21:06:36 +0530

    Add files via upload

---
+++ Setbit.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Setbit{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int pos=sc.nextInt();
+        if((a&&(<<pos))!=0){
+            System.out.println("set bit");
+        }
+        else{
+            System.out.println("not a set bit");
+        }
+    }
+}
