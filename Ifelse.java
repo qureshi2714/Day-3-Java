fa3970bc31963bc3430278a333613c90aa14e3ab
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 21:13:05 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ifelse{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a&1)==0)
+        {
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
